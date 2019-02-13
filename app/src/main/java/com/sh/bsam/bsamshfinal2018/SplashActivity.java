3462bb3afdc2e9ea761450214a6dbb5d0555c0e5
Owner <alsalamcse>
2019-02-13 09:40:22 +0200

    SPLASH ACTIVITY LOADING LOG..

--- app/src/main/java/com/sh/bsam/bsamshfinal2018/SplashActivity.java
+++ app/src/main/java/com/sh/bsam/bsamshfinal2018/SplashActivity.java
@@ -1,11 +1,12 @@
 package com.sh.bsam.bsamshfinal2018;
 
+import android.content.Intent;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class SplashActivity extends AppCompatActivity {
-    private final int SPLASH_DISPLAY_LENGTH = 1000;
+    private final int SPLASH_DISPLAY_LENGTH = 5000;
 
 
     @Override
@@ -15,6 +16,9 @@ public class SplashActivity extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
+                Intent intent = new Intent(SplashActivity.this,logActivity.class);
+                SplashActivity.this.startActivity(intent);
+                SplashActivity.this.finish();
 
             }
         }, SPLASH_DISPLAY_LENGTH);

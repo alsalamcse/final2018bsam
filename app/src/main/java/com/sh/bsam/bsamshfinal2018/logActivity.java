d283014d4a0966a4f25eadaf5cbcb763d781ea5f
Owner <alsalamcse>
2019-02-13 10:47:22 +0200

    SPLASH ACTIVITY LOADING LOG...

--- app/src/main/java/com/sh/bsam/bsamshfinal2018/logActivity.java
+++ app/src/main/java/com/sh/bsam/bsamshfinal2018/logActivity.java
@@ -1,14 +1,46 @@
 package com.sh.bsam.bsamshfinal2018;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
 
 public class logActivity extends AppCompatActivity {
+    private EditText etEmail;
+    private EditText etPassword;
+    private Button btnSignUp;
+    private Button btnSignIn;
+    private RadioButton rbtnStayActive;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log);
 
+        etEmail = (EditText) findViewById(R.id.etEmail);
+        etPassword = (EditText) findViewById(R.id.etPassword);
+        btnSignIn = (Button) findViewById(R.id.btnSingIn);
+        btnSignUp = (Button) findViewById(R.id.btnSignUp);
+        rbtnStayActive = (RadioButton) findViewById(R.id.rbtnStayActive);
+
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(logActivity.this,SignUp.class);
+
+            }
+        });
+
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(logActivity.this,logActivity.class);
+            }
+        });
+
     }
+
 }

0e9d25d0b4812e49075b51c938973519a4b16883
Owner <alsalamcse>
2019-03-03 13:21:03 +0200

    SPLASH ACTIVITY LOADING LOG... sign up done list view ...

---
+++ app/src/main/java/com/sh/bsam/bsamshfinal2018/AllDates.java
@@ -0,0 +1,15 @@
+package com.sh.bsam.bsamshfinal2018;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ListView;
+
+public class AllDates extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_all_dates);
+
+    }
+}
--- app/src/main/java/com/sh/bsam/bsamshfinal2018/logActivity.java
+++ app/src/main/java/com/sh/bsam/bsamshfinal2018/logActivity.java
@@ -1,18 +1,26 @@
 package com.sh.bsam.bsamshfinal2018;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class logActivity extends AppCompatActivity {
     private EditText etEmail;
     private EditText etPassword;
     private Button btnSignUp;
-    private Button btnSignIn;
+    private Button btnSignIn,btnTheBarber;
+    private FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +31,8 @@ public class logActivity extends AppCompatActivity {
         etPassword = (EditText) findViewById(R.id.etPassword);
         btnSignIn = (Button) findViewById(R.id.btnSingIn);
         btnSignUp = (Button) findViewById(R.id.btnSignUp);
+        btnTheBarber = (Button) findViewById(R.id.btnTheBarber);
+
 
         btnSignUp.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -35,10 +45,34 @@ public class logActivity extends AppCompatActivity {
         btnSignIn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(logActivity.this,logActivity.class);
+                signIn(etEmail.getText().toString(),etPassword.getText().toString());
+            }
+        });
+        btnTheBarber.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(logActivity.this,SignUp.class);
+
             }
         });
 
     }
+    private void signIn(String email, String passw) {
+        auth.signInWithEmailAndPassword(email, passw).addOnCompleteListener(logActivity.this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    Toast.makeText(logActivity.this, "signIn Successful", Toast.LENGTH_SHORT).show();
+                    Intent i = new Intent(getApplicationContext(), PickDateActivity.class);
+                    startActivity(i);
+                    finish();
+                } else {
+                    Toast.makeText(logActivity.this, "signIn Failed." + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                    task.getException().printStackTrace();
+                }
+            }
+        });
+    }
+
 
 }

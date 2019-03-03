4a5c0b7b17338354326925131293c654941843f6
Owner <alsalamcse>
2019-03-03 12:50:28 +0200

    SPLASH ACTIVITY LOADING LOG... sign up done

--- app/src/main/java/com/sh/bsam/bsamshfinal2018/SignUp.java
+++ app/src/main/java/com/sh/bsam/bsamshfinal2018/SignUp.java
@@ -1,11 +1,17 @@
 package com.sh.bsam.bsamshfinal2018;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
@@ -51,6 +57,51 @@ public class SignUp extends AppCompatActivity {
         String fname=etFirstName.getText().toString();
         String lname=etLastName.getText().toString();
         String phone=etPhoneNumber.getText().toString();
+        boolean isok = true;
+        if (email.length() < 4|| email.indexOf('@')< 0 || email.indexOf('.') < 0){
+            etEmail.setError("Wrong Email");
+            isok = false;
+        }
+        if (passw1.length()<8){
+            etPassword.setError("Have to be at least 8 char");
+            isok = false;
+        }
+        if (!passw1.equals(passw2)){
+            etConfirmPassword.setError("passwords have to be matched");
+            isok = false;
+        }
+        if (isok){
+            creatAcount(email,passw1);
+
+
+        }
+
     }
 
+    private void creatAcount(String email, String passw) {
+        auth.createUserWithEmailAndPassword(email,passw).addOnCompleteListener(SignUp.this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()){
+                    Toast.makeText(SignUp.this,"Authentication Successful",Toast.LENGTH_SHORT).show();
+                    finish();
+                }
+                else
+                {
+                    Toast.makeText(SignUp.this,"Authentication Failed"+task.getException().toString(),Toast.LENGTH_SHORT).show();
+                    task.getException().printStackTrace();
+                }
+            }
+        });
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SignUp.this,PickDateActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+
 }
+
